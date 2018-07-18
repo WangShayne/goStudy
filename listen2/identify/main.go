ddaaacb7ea7eac61ab0dca95e3edb64846581799
Shayne Wang <1614565666@qq.com>
2018-07-18 13:03:32 +0800

    variable

---
+++ listen2/identify/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 1
+	// var func int = 1   func为保留关键字
+	fmt.Println(a)
+	// fmt.Println(func)
+}
---
+++ listen2/variable/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	/*
+		var a int
+		var b bool
+		var c string
+		var d float32
+	*/
+
+	var (
+		a int
+		b bool
+		c string
+		d float32
+	)
+	fmt.Printf("a=%d ,b=%t, c=%s, d=%f\n", a, b, c, d)
+
+	a = 100
+	b = true
+	c = "ok"
+	d = 0.1
+	fmt.Printf("a=%d ,b=%t, c=%s, d=%f\n", a, b, c, d)
+
+	/*
+		Zhen
+	*/
+
+}
